363629c394319424800128ca0f4462c4b46aa1e3
WildcherryBazzaire <eieghtbitdolphin@gmail.com>
2017-09-20 12:23:46 -1000

    .

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-//const getFilmDate = (id) => 
+var storage = [];
+$("body").append("<input id='input'>");
+
+const buildHtmlElements = (movie) => {
+	var films = storage[0].results;
+	// for (let loop of films){
+	// 		let div = "<div class='container'></div>";
+	// 		$('#films').append(div);
+	// 		let divs = $('.container');
+	// 	}
+	$("body").append("<div id='container'></div>");
+
+}
+
 $("button").click(function(){
 	$.ajax({
 	url: "https://swapi.co/api/films/",
 	type: "GET",
-	success: function (data){
-		$('#films').append(JSON.stringify(data));
+	success: function getFilmData(data){
+		storage.push(JSON.parse(JSON.stringify(data)));
+		buildHtmlElements($("#input").innerText);
 		}
 	})
 })
+
